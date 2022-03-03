8b9fb1e3c3100d5c2b7447bfc907e0068b358bd6
40175607 <amckenna41@qub.ac.uk>
2022-03-03 17:39:59 +0000

    adding manifest, version 2.0.3

--- pySAR/__init__.py
+++ pySAR/__init__.py
@@ -3,5 +3,5 @@ __author__ = 'Adam McKenna, https://github.com/amckenna41'
 __authorEmail__ = 'amckenna41@qub.ac.uk'
 __url__ = 'https://github.com/amckenna41/pySAR'
 __credits__ = ['Adam McKenna']
-__version__ = "1.8.1"
+__version__ = "2.0.4"
 __license__ = 'MIT'
